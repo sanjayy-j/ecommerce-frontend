0ba8830be6391ecb4296ccb8fa7e5e0d0788ba40
sanjayy-j <sanjayjayakumar2006@gmail.com>
2025-06-25 00:35:49 +0530

    Home Page Layout

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
 console.log("E-Commerce Website Loaded");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
